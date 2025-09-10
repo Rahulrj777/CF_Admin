671f8c7e596d74621ac04d535c68b45d3563fb63
Vishnuads <your_email@example.com>
2025-09-10 12:23:05 +0530

    Updated: bug fix and UI change

--- src/App.jsx
+++ src/App.jsx
@@ -478,7 +478,7 @@ function App() {
             </button>
           </div>
 
-          <div className="p-6 pt-18 lg:pt-0">
+          <div className="p-6 pt-18 lg:pt-5">
             <div className="hidden lg:flex justify-end mb-4">
               <button
                 onClick={handleLogout}
